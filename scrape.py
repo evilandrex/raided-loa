e1a60950818d4ad74430d4b2ab654a825df64e67
evilandrex <chowjason1234@gmail.com>
2024-04-30 15:44:31 -0500

    Refactor scrape.py to scrape all

--- scrape.py
+++ scrape.py
@@ -4,42 +4,7 @@ import pandas as pd
 import time
 
 
-@click.group()
-def cli():
-    pass
-
-
-@click.command()
-@click.argument("boss", type=str, required=True)
-@click.argument("gate", type=int, required=False)
-@click.argument("difficulty", type=str, required=False)
-@click.option(
-    "--from-latest/--from-oldest",
-    default=True,
-    help="Start from either the latest or oldest log",
-)
-@click.option(
-    "--from-scratch",
-    default=False,
-    is_flag=True,
-    help="Start from scratch, overwrite cached logs",
-)
-@click.option(
-    "--log-batches",
-    default=20,
-    help="Number of logs to fetch per batch",
-)
-@click.option(
-    "--max-logs",
-    default=100,
-    help="Maximum number of logs to fetch before stopping.",
-)
-@click.option(
-    "--patience",
-    default=20,
-    help="Number of empty calls before stopping.",
-)
-def boss(
+def scrape_log(
     boss: str,
     gate: int = None,
     difficulty: str = None,
@@ -48,12 +13,8 @@ def boss(
     log_batches: int = 20,
     max_logs: int = 100,
     patience: int = 20,
+    force: bool = False,
 ):
-    """
-    Fetch logs for a specific boss, gate, and difficulty.
-
-    BOSS is required, GATE and DIFFICULTY should not be set unless necessary.
-    """
     click.echo(f"Fetching logs for {boss} {gate} {difficulty}")
     click.echo(f"Starting from {'latest' if from_latest else 'oldest'}")
 
@@ -67,7 +28,11 @@ def boss(
         click.echo("")
         click.echo("=== Starting from scratch ===")
         click.echo("WARNING: THIS OVERWRITES OLD LOGS")
-        click.confirm("Are you sure you want to continue?", abort=True)
+        if not force:
+            click.confirm("Are you sure you want to continue?", abort=True)
+        else:
+            click.echo("Continuing without confirmation in three seconds.")
+            time.sleep(3)
 
         df = pd.DataFrame()
         oldIDs = []
@@ -137,7 +102,139 @@ def boss(
     click.echo(f"Time elapsed: {end - start:.2f} seconds")
 
 
+@click.group()
+def cli():
+    pass
+
+
+@click.command()
+@click.argument("boss", type=str, required=True)
+@click.argument("gate", type=int, required=False)
+@click.argument("difficulty", type=str, required=False)
+@click.option(
+    "--from-latest/--from-oldest",
+    default=True,
+    help="Start from either the latest or oldest log",
+)
+@click.option(
+    "--from-scratch",
+    default=False,
+    is_flag=True,
+    help="Start from scratch, overwrite cached logs",
+)
+@click.option(
+    "--log-batches",
+    default=20,
+    help="Number of logs to fetch per batch",
+)
+@click.option(
+    "--max-logs",
+    default=100,
+    help="Maximum number of logs to fetch before stopping.",
+)
+@click.option(
+    "--patience",
+    default=20,
+    help="Number of empty calls before stopping.",
+)
+def boss(
+    boss: str,
+    gate: int = None,
+    difficulty: str = None,
+    from_latest: bool = True,
+    from_scratch: bool = False,
+    log_batches: int = 20,
+    max_logs: int = 100,
+    patience: int = 20,
+):
+    """
+    Fetch logs for a specific boss, gate, and difficulty.
+
+    BOSS is required, GATE and DIFFICULTY should not be set unless necessary.
+    """
+    scrape_log(
+        boss,
+        gate,
+        difficulty,
+        from_latest,
+        from_scratch,
+        log_batches,
+        max_logs,
+        patience,
+    )
+
+
 cli.add_command(boss)
+
+
+@click.command()
+@click.option(
+    "--from-latest/--from-oldest",
+    default=True,
+    help="Start from either the latest or oldest log",
+)
+@click.option(
+    "--from-scratch",
+    default=False,
+    is_flag=True,
+    help="Start from scratch, overwrite cached logs",
+)
+@click.option(
+    "--log-batches",
+    default=20,
+    help="Number of logs to fetch per batch",
+)
+@click.option(
+    "--max-logs",
+    default=100,
+    help="Maximum number of logs to fetch before stopping.",
+)
+@click.option(
+    "--patience",
+    default=20,
+    help="Number of empty calls before stopping.",
+)
+def all(
+    from_latest: bool = True,
+    from_scratch: bool = False,
+    log_batches: int = 20,
+    max_logs: int = 100,
+    patience: int = 20,
+):
+    """Scrape all bosses."""
+    # Start timer
+    start = time.time()
+
+    # Build a list of filter args
+    bossArgs = []
+    for bossName, info in api.BOSSES.items():
+        # If info is empty, it's probably a guardian
+        if info == {}:
+            bossArgs += [{"boss": bossName}]
+        else:
+            # Get the keys for difficulty
+            for diff, gates in info.items():
+                for gate in gates:
+                    bossArgs += [{"boss": bossName, "gate": gate, "difficulty": diff}]
+
+    # Loop through bossArgs
+    for kwargs in bossArgs:
+        scrape_log(
+            **kwargs,
+            from_latest=from_latest,
+            from_scratch=from_scratch,
+            log_batches=log_batches,
+            max_logs=max_logs,
+            patience=patience,
+            force=True,
+        )
+
+    # End timer
+    end = time.time()
+    click.echo(f"Time elapsed: {end - start:.2f} seconds")
+
+
+cli.add_command(all)
 
 if __name__ == "__main__":
     cli()
