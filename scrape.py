37ce240302bb4039ce2f41bc9c436e2ee58e7b63
evilandrex <chowjason1234@gmail.com>
2024-05-07 09:38:56 -0500

    unify api calls

--- scrape.py
+++ scrape.py
@@ -175,178 +175,54 @@ def boss(
     # TODO: Scrap until date
     # TODO: Update specific classes
     # TODO: Update specific IDs
-    scrape_log(
-        boss,
-        gate,
-        difficulty,
-        from_latest,
-        from_scratch,
-        log_batches,
-        max_logs,
-        patience,
-        verbose,
-    )
+    if boss == "all":
+        # Start timer
+        start = time.time()
+
+        # Build a list of filter args
+        bossArgs = []
+        for bossName, info in api.BOSSES.items():
+            # If info is empty, it's probably a guardian
+            if info == {}:
+                bossArgs += [{"boss": bossName}]
+            else:
+                # Get the keys for difficulty
+                for diff, gates in info.items():
+                    for gate in gates:
+                        bossArgs += [{"boss": bossName, "gate": gate, "difficulty": diff}]
+
+        # Loop through bossArgs
+        for kwargs in bossArgs:
+            scrape_log(
+                **kwargs,
+                from_latest=from_latest,
+                from_scratch=from_scratch,
+                log_batches=log_batches,
+                max_logs=max_logs,
+                patience=patience,
+                force=True,
+                verbose=verbose,
+            )
+
+        # End timer
+        end = time.time()
+        click.echo(f"Time elapsed: {end - start:.2f} seconds")
+    else:
+        scrape_log(
+            boss,
+            gate,
+            difficulty,
+            from_latest,
+            from_scratch,
+            log_batches,
+            max_logs,
+            patience,
+            verbose,
+        )
 
 
 cli.add_command(boss)
 
-
-@click.command()
-@click.option(
-    "--from-latest/--from-oldest",
-    default=True,
-    help="Start from either the latest or oldest log",
-)
-@click.option(
-    "--from-scratch",
-    default=False,
-    is_flag=True,
-    help="Start from scratch, overwrite cached logs",
-)
-@click.option(
-    "--log-batches",
-    default=20,
-    help="Number of logs to fetch per batch",
-)
-@click.option(
-    "--max-logs",
-    default=100000000,
-    help="Maximum number of logs to fetch before stopping",
-)
-@click.option(
-    "--patience",
-    default=100000000,
-    help="Number of empty calls before stopping",
-)
-@click.option(
-    "--verbose",
-    "-v",
-    default=False,
-    is_flag=True,
-    help="Print extra information",
-)
-def all(
-    from_latest: bool = True,
-    from_scratch: bool = False,
-    log_batches: int = 20,
-    max_logs: int = 100000000,
-    patience: int = 100000000,
-    verbose: bool = False,
-):
-    """Scrape all bosses."""
-    # Start timer
-    start = time.time()
-
-    # Build a list of filter args
-    bossArgs = []
-    for bossName, info in api.BOSSES.items():
-        # If info is empty, it's probably a guardian
-        if info == {}:
-            bossArgs += [{"boss": bossName}]
-        else:
-            # Get the keys for difficulty
-            for diff, gates in info.items():
-                for gate in gates:
-                    bossArgs += [{"boss": bossName, "gate": gate, "difficulty": diff}]
-
-    # Loop through bossArgs
-    for kwargs in bossArgs:
-        scrape_log(
-            **kwargs,
-            from_latest=from_latest,
-            from_scratch=from_scratch,
-            log_batches=log_batches,
-            max_logs=max_logs,
-            patience=patience,
-            force=True,
-            verbose=verbose,
-        )
-
-    # End timer
-    end = time.time()
-    click.echo(f"Time elapsed: {end - start:.2f} seconds")
-
-
-cli.add_command(all)
-
-
-@click.command()
-@click.option(
-    "--from-latest/--from-oldest",
-    default=True,
-    help="Start from either the latest or oldest log",
-)
-@click.option(
-    "--from-scratch",
-    default=False,
-    is_flag=True,
-    help="Start from scratch, overwrite cached logs",
-)
-@click.option(
-    "--log-batches",
-    default=20,
-    help="Number of logs to fetch per batch",
-)
-@click.option(
-    "--max-logs",
-    default=100000000,
-    help="Maximum number of logs to fetch before stopping",
-)
-@click.option(
-    "--patience",
-    default=100000000,
-    help="Number of empty calls before stopping",
-)
-@click.option(
-    "--verbose",
-    "-v",
-    default=False,
-    is_flag=True,
-    help="Print extra information",
-)
-def update(
-    from_latest: bool = True,
-    from_scratch: bool = False,
-    log_batches: int = 20,
-    max_logs: int = 100000000,
-    patience: int = 100000000,
-    verbose: bool = False,
-):
-    """Update relevant bosses."""
-    # Start timer
-    start = time.time()
-
-    # Build a list of filter args
-    bossArgs = []
-    for bossName, info in api.KEEP_UPDATED.items():
-        # If info is empty, it's probably a guardian
-        if info == {}:
-            bossArgs += [{"boss": bossName}]
-        else:
-            # Get the keys for difficulty
-            for diff, gates in info.items():
-                for gate in gates:
-                    bossArgs += [{"boss": bossName, "gate": gate, "difficulty": diff}]
-
-    # Loop through bossArgs
-    for kwargs in bossArgs:
-        scrape_log(
-            **kwargs,
-            from_latest=from_latest,
-            from_scratch=from_scratch,
-            log_batches=log_batches,
-            max_logs=max_logs,
-            patience=patience,
-            force=True,
-            verbose=verbose,
-        )
-
-    # End timer
-    end = time.time()
-    click.echo(f"Time elapsed: {end - start:.2f} seconds")
-
-
-cli.add_command(update)
 
 
 if __name__ == "__main__":
