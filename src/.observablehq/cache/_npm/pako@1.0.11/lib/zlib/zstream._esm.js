c2df61ddc767273c4d2e268a8e65f3a544a9ff31
evilandrex <chowjason1234@gmail.com>
2024-08-30 00:55:11 -0700

    oopsie deps

---
+++ src/.observablehq/cache/_npm/pako@1.0.11/lib/zlib/zstream._esm.js
@@ -0,0 +1,7 @@
+/**
+ * Bundled by jsDelivr using Rollup v2.79.1 and Terser v5.19.2.
+ * Original file: /npm/pako@1.0.11/lib/zlib/zstream.js
+ *
+ * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
+ */
+var t=function(){this.input=null,this.next_in=0,this.avail_in=0,this.total_in=0,this.output=null,this.next_out=0,this.avail_out=0,this.total_out=0,this.msg="",this.state=null,this.data_type=2,this.adler=0};export{t as default};
---
+++ src/.observablehq/cache/_npm/zlib@1.0.5/_esm.js
@@ -0,0 +1,6 @@
+/**
+ * Failed to bundle using Rollup v2.79.1: failed to resolve an internal import.
+ * If you believe this to be an issue with jsDelivr, and not with the package itself, please open an issue at https://github.com/jsdelivr/jsdelivr
+ */
+
+ throw new Error('Failed to bundle using Rollup v2.79.1: failed to resolve an internal import. If you believe this to be an issue with jsDelivr, and not with the package itself, please open an issue at https://github.com/jsdelivr/jsdelivr');
